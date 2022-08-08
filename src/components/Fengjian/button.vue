1995b0944e0b618012e69b2361706de81da168d1
18God <1226343906@qq.com>
2022-08-08 10:26:07 +0800

    对账系统完成98%

---
+++ src/components/Fengjian/button.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="btn" @click="$emit('Submit')">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+
+  created() {},
+
+  methods: {},
+}
+</script>
+
+<style scoped>
+.btn {
+  width: 90px;
+  height: 40px;
+  border-radius: 5px;
+  background-color: #5f84ff;
+  line-height: 40px;
+  font-size: 14px;
+  text-align: center;
+  cursor: pointer;
+  color: #fff;
+}
+</style>
